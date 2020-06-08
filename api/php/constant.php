313bc0b02c0900e0641440d987beac2c1813b728
gouderg <victor.illien974@gmail.com>
2020-06-08 22:47:37 +0200

    Squelette v2, sql, auth

---
+++ api/php/constant.php
@@ -0,0 +1,7 @@
+<?php 
+	//Constantes nécessaires pour la connexion à la base de données
+	define('DB_USER', 'leopoldUSER');
+	define('DB_PASSWORD', 'phpInMyHeart333!!!');
+	define('DB_NAME', 'projet_web_IV_PL');
+	define('DB_SERVER', 'localhost');
+?>
