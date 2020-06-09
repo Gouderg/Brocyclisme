100a068768f40f7ef78ee5024c623c67347d6142
gouderg <victor.illien974@gmail.com>
2020-06-09 13:23:31 +0200

    test

--- front/html/gestion_coureurs.php
+++ front/html/gestion_coureurs.php
@@ -2,10 +2,27 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+	<script type="text/javascript" src="../js/ajax.js" defer></script>
+	<script type="text/javascript" src="../js/coureurs.js" defer></script>
 	<title>Brocyclisme</title>
 </head>	
 <body>
 	<?php require "header.html" ?>
+
+
+	<section id="cyclistes" style="display: none;">
+        <div id="liste-cyclistes" class="container"> </div>
+            <form>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> Listes - Cycliste></span>
+                    </div> 
+                </div>
+                <br>
+            </form>
+   	</section>
+
 	<?php require "footer.html" ?>
+
 </body>
 </html>
