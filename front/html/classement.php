53a245b450bc7c090ae95d435d9ddfd153899af8
gouderg <victor.illien974@gmail.com>
2020-06-09 13:24:24 +0200

    test

--- front/html/classement.php
+++ front/html/classement.php
@@ -2,10 +2,12 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
+
 	<title>Brocyclisme</title>
 </head>	
 <body>
 	<?php require "header.html" ?>
 	<?php require "footer.html" ?>
+
 </body>
 </html>
